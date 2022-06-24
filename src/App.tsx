12077007afd3573cfa94896cb589cd97e2e30eb3
Ali-Eldeba <alieldeba375@gmail.com>
2022-06-24 14:11:49 +0200

    adding the structure ✔

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1 className="text-center text-3xl text-blue-500">HI AHMED</h1>
+    </>
+  );
+}
+
+export default App;
